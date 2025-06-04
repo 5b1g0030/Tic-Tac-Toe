967f5eaee41be53e61dff933d17d5c22e94dd769
5b1g0030 <5b1g0030@stust.edu.tw>
2025-06-04 14:40:18 +0800

    加入遊戲判斷程式(不完整)，已解決css無法引用問題

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,28 +1,68 @@
 import React from 'react';
 import Square from './Square';
+import './tictactoe.css';
 
-function Board(){
+function Board(xIsNext, squares, onPlay) {
+    const handleClick = (i) => {
+        // console.log('Square clicked:', i);
+        const nextSquares = squares.slice();
+        if (nextSquares[i] || calculateWinner(nextSquares)) {
+            return; // 如果方格已經被點擊或遊戲已經結束，則不做任何操作
+        }
+        // const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = 'X'; // 如果是 X 的回合，則在方格中放置 X
+        }
+        else {
+            nextSquares[i] = 'O'; // 如果是 O 的回合，則在方格中放置 O
+        }
+    }
+
     return (
         <>
             <div className='stuats'>
                 <div className='board-row'>
-                    <Square value={0} onSquaerClick={()=>{}}/>
-                    <Square value={1} onSquaerClick={()=>{}}/>
-                    <Square value={2} onSquaerClick={()=>{}}/>
+                    <Square value={squares[0]} onSquaerClick={() => handleClick(0)} />
+                    <Square value={squares[1]} onSquaerClick={() => handleClick(1)} />
+                    <Square value={squares[2]} onSquaerClick={() => handleClick(2)} />
                 </div>
                 <div className='board-row'>
-                    <Square value={3} onSquaerClick={()=>{}}/>
-                    <Square value={4} onSquaerClick={()=>{}}/>
-                    <Square value={5} onSquaerClick={()=>{}}/>
+                    <Square value={squares[3]} onSquaerClick={() => handleClick(3)} />
+                    <Square value={squares[4]} onSquaerClick={() => handleClick(4)} />
+                    <Square value={squares[5]} onSquaerClick={() => handleClick(5)} />
                 </div>
                 <div className='board-row'>
-                    <Square value={6} onSquaerClick={()=>{}}/>
-                    <Square value={7} onSquaerClick={()=>{}}/>
-                    <Square value={8} onSquaerClick={()=>{}}/>
+                    <Square value={squares[6]} onSquaerClick={() => handleClick(6)} />
+                    <Square value={squares[7]} onSquaerClick={() => handleClick(7)} />
+                    <Square value={squares[8]} onSquaerClick={() => handleClick(8)} />
                 </div>
             </div>
         </>
     );
 }
+
+// 判斷是否連成直線
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
+
 
 export default Board;
--- src/component/TicTacToe.jsx
+++ src/component/TicTacToe.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import './tictactoe.css';
 
 function TicTacToe(){
+    const [history, setHistory] = useState([Array(9).fill(null)]); // 初始化歷史紀錄，包含一個空的方格陣列
+    const [currentMove, setCurrentMove] = useState(0); // 當前的移動步數，初始為 0
+    const xIsNext = currentMove % 2 === 0; // 判斷當前是 X 還是 O 的回合
+    const currentSquares = history[currentMove]; // 獲取當前步數的方格狀態
+    
+    const handlePlay = (nextSquares) => {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    };
+
+    const jumpTo = (nextMove) => {
+        setCurrentMove(nextMove);
+    };
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0){
+            description = 'Go to move #' + move;
+        }
+        else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    })
     return(
         <div className='tic-tac-toe'>
             <h1>tic tac toe</h1>
             <div className='game-borad'>
-                <Board/>
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className='game-info'>
+                <h4>狀態</h4>
+                <ol>{moves}</ol> 
             </div>
         </div>
     )
 }
+
+
 
 export default TicTacToe;
